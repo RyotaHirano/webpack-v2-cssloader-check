7d194f735c206573e42c988218a2f724f6fca6cc
ryotahirano <hirano@tsumikiinc.com>
2017-10-24 14:03:45 +0900

    initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+import { resolve } from 'path';
+const rootResolve = pathname => resolve(__dirname, pathname);
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '',
+    filename: 'assets/js/[name].js'
+  },
+  externals: {
+    //
+  },
+  resolve: {
+    extensions: ['.js', '.scss'],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: `${rootResolve('src/html/index.pug')}`,
+    }),
+    new ExtractTextPlugin({
+      filename: 'assets/css/style.[hash].css',
+      disable: process.env.NODE_ENV !== 'production',
+      allChunks: true
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        exclude: /node_modules/,
+        use: 'pug-loader'
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 2 } },
+            'postcss-loader',
+            'sass-loader',
+          ]
+        })
+      }
+    ]
+  },
+  devServer: {
+    contentBase: rootResolve('docs'),
+    publicPath: '/',
+    hot: true,
+    host: '0.0.0.0'
+  }
+};
